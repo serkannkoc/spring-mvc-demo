2de57df19bfe40318d8ee05272129d2c548a29d7
Serkan Koç <serkan.koc@marun.edu.tr>
2022-11-03 19:50:21 +0300

    Section 17: Spring MVC Form Validation - Creating Custom Validation Rules

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,19 +1,31 @@
 package com.luv2code.springdemo.mvc;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+
 import javax.validation.constraints.*;
 
 public class Customer {
     private String firstName;
     @NotNull(message = "is required")
-    @Size(min = 1,message = "is required")
+    @Size(min = 1, message = "is required")
     private String lastName;
 
     @NotNull(message = "is required")
-    @Min(value = 0,message = "must be greater than or equal to zero")
-    @Max(value = 10,message = "must be less than or equal to 10")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
     private Integer freePasses;
-    @Pattern(regexp = "^[a-zA-Z0-9]{5}",message = "only 5 chars/digits")
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+    @CourseCode(value = "TOPS",message = "must start with TOPS")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getPostalCode() {
         return postalCode;
